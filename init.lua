17a4b590192738f59a8cc123eddfb7e0e98c2a22
0ju1c3 <otapadia@gmail.com>
2025-01-04 08:56:42 +0530

    feat: added undotree

--- init.lua
+++ init.lua
@@ -1087,6 +1087,10 @@ require("lazy").setup({
 			--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 		end,
 	},
+	{
+		"mbbill/undotree",
+		vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle),
+	},
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
